8e6ddde0435d568d233556886a04c05cce32ba87
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-08-10 22:00:58 -0500

    chore: add tests for `util` functions `assert_all()`, `assert_true()`, `blend_color()`

---
+++ tests/test_util.py
@@ -0,0 +1,43 @@
+import pytest
+from geometry import Tuple4
+
+from positionpolling import util
+
+
+def test_assert_all() -> None:
+    util.assert_all((1, 'true', True, [1]))
+
+    with pytest.raises(AssertionError, match=r"predicate \(''\) is False"):
+        util.assert_all((1, '', False, []))
+
+def test_assert_true() -> None:
+    util.assert_true(True)  # noqa: FBT003
+    util.assert_true(1)
+    util.assert_true('true')
+
+    with pytest.raises(AssertionError):
+        util.assert_true(False)  # noqa: FBT003
+
+    with pytest.raises(AssertionError):
+        util.assert_true(0)
+
+    with pytest.raises(AssertionError):
+        util.assert_true('')
+
+@pytest.mark.parametrize(('a', 'b', 'delta', 'expected'),
+    [
+        ((255, 0, 0, 255), (  0, 255,   0, 255), 25, (191,  63,   0, 255)),
+        ((127, 0, 0, 255), (  0, 255,   0, 255), 25, ( 95,  63,   0, 255)),
+        ((  0, 0, 0, 255), (  0, 255,   0, 255), 25, (  0,  63,   0, 255)),
+        ((255, 0, 0, 255), (  0, 255,   0, 255), 50, (127, 127,   0, 255)),
+        ((127, 0, 0, 255), (  0, 255,   0, 255), 50, ( 63, 127,   0, 255)),
+        ((  0, 0, 0, 255), (  0, 255,   0, 255), 50, (  0, 127,   0, 255)),
+        ((255, 0, 0, 255), (  0, 255,   0, 255), 75, ( 63, 191,   0, 255)),
+        ((127, 0, 0, 255), (  0, 255,   0, 255), 75, ( 31, 191,   0, 255)),
+        ((  0, 0, 0, 255), (  0, 255,   0, 255), 75, (  0, 191,   0, 255)),
+    ],
+)
+def test_blend_color(a: Tuple4[int], b: Tuple4[int], delta: float, expected: Tuple4[float]) -> None:
+    assert util.blend_color(a, b, delta) == expected
+    assert util.blend_color(a, b, 0) == a
+    assert util.blend_color(a, b, 100) == b
