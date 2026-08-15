635077f089a6ea5f0c35ecf878685704c76957b3
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-08-15 13:33:02 -0500

    feat: add module `errors` with `ValueWarning` warning class

---
+++ positionpolling/errors.py
@@ -0,0 +1,4 @@
+"""Exceptions and warnings for positionpolling."""
+
+class ValueWarning(Warning):
+    """Emitted when an improper value was received, but a full ``ValueError`` does not need to be raised."""
