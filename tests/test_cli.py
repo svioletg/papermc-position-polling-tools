8f449320c4cfcd8ad7b0a39ae11e1359cca997be
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-09-04 14:33:46 -0500

    tests: add tests for main parser and `render trail` parser

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,17 @@
 from argparse import Action, ArgumentParser, BooleanOptionalAction
-from typing import Annotated
+from pathlib import Path
+from typing import Annotated, Any
 from unittest.mock import patch
+from uuid import uuid4
 
+import pytest
 from pydantic import BaseModel
 
 from positionpolling import cli
+from positionpolling.const import DEFAULT_LOGS_DIR
 from positionpolling.models import CliOpt
 
+PLAYERS: list[str] = [str(uuid4()) for _ in range(10)]
 
 def test_add_args_from_render_opt() -> None:
     class MockRenderOpt(BaseModel):
@@ -37,3 +42,90 @@ def test_add_args_from_render_opt() -> None:
     opt_number_list = action_map['number_list']
     assert opt_number_list.option_strings == ['--number-list']
     assert opt_number_list.type is cli.comma_split
+
+MAIN_PARSER_DEFAULTS: dict[str, Any] = {
+    'version': False,
+    'log_level': 'INFO',
+    'log_file': DEFAULT_LOGS_DIR,
+    'no_color': False,
+    'yes': False,
+}
+
+@pytest.mark.parametrize(('args', 'parsed_expected'),
+    [
+        ([], {}),
+        (['--log-level', 'debug'], {'log_level': 'DEBUG'}),
+        (['--logfile', 'mylogs'], {'log_file': Path('mylogs')}),
+        (['--no-logfile'], {'log_file': False}),
+    ],
+)
+def test_parse_main(args: list[str], parsed_expected: dict[str, Any]) -> None:
+    parsed = cli.main_parser.parse_args(args)
+    parsed_expected = MAIN_PARSER_DEFAULTS | parsed_expected
+
+    for name, value in parsed_expected.items():
+        assert getattr(parsed, name) == value
+
+RENDER_TRAIL_PARSER_DEFAULTS: dict[str, Any] = {
+    'input': ...,
+    'out': None,
+    'video': None,
+    'player': None,
+    'desat_per_frame': 0.95,
+}
+
+@pytest.mark.parametrize(('args', 'parsed_expected'),
+    params := [
+        (
+            ['--input', 'data.db', '--out', 'trail.png'],
+            {
+                'input': 'data.db',
+                'out': Path('trail.png'),
+            },
+        ),
+        (
+            ['--input', 'data.db', '--video', 'trail.mp4'],
+            {
+                'input': 'data.db',
+                'video': Path('trail.mp4'),
+            },
+        ),
+        (
+            ['--input', 'data.db', '--out', 'trail.png', '--video', 'trail.mp4'],
+            {
+                'input': 'data.db',
+                'out': Path('trail.png'),
+                'video': Path('trail.mp4'),
+            },
+        ),
+        (
+            ['--input', 'data.db', '--player', PLAYERS[0]],
+            {
+                'input': 'data.db',
+                'player': [PLAYERS[0]],
+            },
+        ),
+        # Test multiple players given to single option
+        (
+            ['--input', 'data.db', '--player', *PLAYERS],
+            {
+                'input': 'data.db',
+                'player': PLAYERS,
+            },
+        ),
+        # Test multiple players given as separate options
+        (
+            ['--input', 'data.db'] + [i for p in PLAYERS for i in ('--player', p)],
+            {
+                'input': 'data.db',
+                'player': PLAYERS,
+            },
+        ),
+    ],
+)
+def test_parse_render_trail(args: list[str], parsed_expected: dict[str, Any]) -> None:
+    parsed = cli.parser_render_trail.parse_args(args)
+    parsed_expected = RENDER_TRAIL_PARSER_DEFAULTS | parsed_expected
+
+    for name, value in parsed_expected.items():
+        assert getattr(parsed, name) == value
